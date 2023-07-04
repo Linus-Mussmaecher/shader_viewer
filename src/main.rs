eee3a3f693c775843015083ae5b336ce157ebdd8
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-07-04 22:00:49 +0200

    Added fullscreen

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,46 @@ fn main() {
     let event_loop = EventLoop::new();
 
     let window = WindowBuilder::new()
+        .with_resizable(false)
+        .with_inner_size(winit::dpi::Size::Physical(winit::dpi::PhysicalSize {
+            width: 630,
+            height: 500,
+        }))
         .build(&event_loop)
         .expect("Could not init window.");
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested => {
-                *control_flow = ControlFlow::Exit;
-            }
-            WindowEvent::CursorEntered { device_id: _ } => {
-                println!("Entered!");
-            }
+    event_loop.run(
+        move |event, _event_loop_window_target, control_flow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                WindowEvent::CursorEntered { device_id: _ } => {
+                    println!("Entered!");
+                }
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::F11),
+                            ..
+                        },
+                    ..
+                } => {
+                    println!("{:?}", window.fullscreen());
+                    if window.fullscreen().is_some() {
+                        window.set_fullscreen(None);
+                    } else {
+                        window.set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)));
+                    }
+                    println!("{:?}", window.fullscreen());
+                }
+                _ => {}
+            },
             _ => {}
         },
-        _ => {}
-    });
+    );
 }
